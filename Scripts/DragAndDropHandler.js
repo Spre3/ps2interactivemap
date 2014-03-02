9c220e5d1343316a806e0a0c826c7a305eb865fa
Spre3 <robertocpsilva@gmail.com>
2014-03-02 15:39:54 +0000

    Initial commit

---
+++ Scripts/DragAndDropHandler.js
@@ -0,0 +1,42 @@
+﻿$(function () {
+    $(".drag-item").draggable({
+        helper: getHelper,
+        cursor: 'move',
+        revert: revert,
+        cursorAt: { top: 10, left: 10 }
+    })    
+    ;
+   
+    $("#mapContainer").droppable({
+        tolerance:"pointer",
+        drop: function (event, ui) {            
+            var $canvas = $(this);
+            if (!ui.draggable.hasClass('canvas-element')) {
+                var $canvasElement = $(ui.draggable).find('span').clone();
+                $canvasElement.addClass('canvas-element');
+                $canvasElement.draggable({
+                    containment: '#mapContainer'
+                });
+                $canvas.append($canvasElement);
+                $canvasElement.css({
+                    left: (ui.offset.left - $canvas.offset().left),
+                    top: ui.offset.top - $canvas.offset().top,
+                    position: 'absolute',
+                    marginRight: 0
+                });
+            }
+        }
+    });
+
+
+});
+function revert(dropped){
+    if (!dropped)
+        return true;
+    return false;
+};
+function getHelper(event) {
+    return $(event.currentTarget).find('span').clone();
+  
+   
+}
